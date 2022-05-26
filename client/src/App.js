8571658ad90dd020c697ba55499066917c317fc1
Fathur Danu <fathurdanu@gmail.com>
2022-05-26 15:27:42 +0700

    Nambahin Footer.js dan Card.js

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Footer from './components/Footer';
+import Card from './components/Card'
+
+function App() {
+  return (
+    <>
+      <div>Hello world</div>
+
+      <div>
+        {/* <Footer></Footer> */}
+        <Card></Card>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ client/tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors:{
+        'accentColor': '#FFCC1D',
+        'lightColor' : '#E8E8CC', 
+        'midColor' : '#116530',  
+        'darkColor' : '#0B4619',
+      }
+    },
+  },
+  plugins: [],
+}
