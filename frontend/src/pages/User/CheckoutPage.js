55249bf7b6853e961ed36ff4c3407a12d578587c
Fathur Danu <fathurdanu@gmail.com>
2022-06-01 18:42:44 +0700

    merging stripe, login, redux

--- frontend/src/pages/User/CheckoutPage.js
+++ frontend/src/pages/User/CheckoutPage.js
@@ -1,203 +1,253 @@
-import React from "react";
-import { useNavigate, Link } from "react-router-dom";
+import React, { useState } from "react";
+
+import { useNavigate } from "react-router-dom";
+import Modal from "react-modal";
 import Header from "../../components/Header";
+import StripeContainer from "../../components/StripeContainer";
 
 const CheckoutPage = () => {
   const navigate = useNavigate();
+  let subtitle;
+  const [openModal, setOpenModal] = useState(false);
+
+  const customStyles = {
+    overlay: {
+      position: "fixed",
+      top: 0,
+      left: 0,
+      right: 0,
+      bottom: 0,
+      backgroundColor: "rgba(55, 49, 52, 0.5)",
+    },
+    content: {
+      top: "50%",
+      left: "50%",
+      right: "auto",
+      bottom: "auto",
+      marginRight: "-50%",
+      transform: "translate(-50%, -50%)",
+    },
+  };
+
+  function afterOpenModal() {
+    // references are now sync'd and can be accessed.
+    subtitle.style.color = "#f00";
+  }
+
+  function closeModal() {
+    setOpenModal(false);
+  }
+
   return (
-    <div className="container mx-auto">
+    <div class="container mx-auto">
       <Header />
-      <div className="w-full bg-white">
-        <h1 className="text-xl text-center font-semibold py-3">Checkout</h1>
-        <h2 className="text-lg text-left font-semibold px-24 py-3">Items Bought</h2>
-        <hr className="mx-24" />
-        <div className="px-24 py-3">
-          <table className="w-full text-sm lg:text-base" cellspacing="0">
+      <Modal
+        isOpen={openModal}
+        onAfterOpen={afterOpenModal}
+        style={customStyles}
+        contentLabel="Example Modal"
+      >
+        <StripeContainer />
+        <div align="center" class="py-5">
+          <button
+            class="bg-darkColor text-lightColor p-3 rounded font-semibold"
+            onClick={closeModal}
+          >
+            Close
+          </button>
+        </div>
+      </Modal>
+      <div class="w-full bg-white">
+        <h1 class="text-xl text-center font-semibold py-3">Checkout</h1>
+        <h2 class="text-lg text-left font-semibold px-24 py-3">Items Bought</h2>
+        <hr class="mx-24" />
+        <div class="px-24 py-3">
+          <table class="w-full text-sm lg:text-base" cellspacing="0">
             <thead>
-              <tr className="h-12 uppercase">
-                <th className="hidden md:table-cell"></th>
-                <th className="text-left">Product</th>
-                <th className="lg:text-right text-left pl-5 lg:pl-0">
-                  <span className="lg:hidden" title="Quantity">
+              <tr class="h-12 uppercase">
+                <th class="hidden md:table-cell"></th>
+                <th class="text-left">Product</th>
+                <th class="lg:text-right text-left pl-5 lg:pl-0">
+                  <span class="lg:hidden" title="Quantity">
                     Qtd
                   </span>
-                  <span className="hidden lg:inline">Quantity</span>
+                  <span class="hidden lg:inline">Quantity</span>
                 </th>
-                <th className="hidden text-right md:table-cell">Unit price</th>
-                <th className="text-right">Total price</th>
+                <th class="hidden text-right md:table-cell">Unit price</th>
+                <th class="text-right">Total price</th>
               </tr>
             </thead>
             <tbody>
               <tr>
-                <td className="hidden pb-4 md:table-cell">
-                  <Link to="#">
+                <td class="hidden pb-4 md:table-cell">
+                  <a href="#">
                     <img
                       src="https://images.pexels.com/photos/61127/pexels-photo-61127.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-                      className="w-40 h-30 rounded"
+                      class="w-40 h-30 rounded"
                       alt="Thumbnail"
                     />
-                  </Link>
+                  </a>
                 </td>
                 <td>
-                  <Link to="#">
-                    <p className="mb-2 md:ml-4">Pisang Cavendish Organik</p>
-                  </Link>
+                  <a href="#">
+                    <p class="mb-2 md:ml-4">Pisang Cavendish Organik</p>
+                  </a>
                 </td>
-                <td className="hidden text-right md:table-cell pb-3">
-                  <span className="text-sm lg:text-base font-medium">5</span>
+                <td class="hidden text-right md:table-cell pb-3">
+                  <span class="text-sm lg:text-base font-medium">5</span>
                 </td>
-                <td className="hidden text-right md:table-cell pb-3">
-                  <span className="text-sm lg:text-base font-medium">10000</span>
+                <td class="hidden text-right md:table-cell pb-3">
+                  <span class="text-sm lg:text-base font-medium">10000</span>
                 </td>
-                <td className="text-right pb-3">
-                  <span className="text-sm lg:text-base font-bold text-darkColor">
+                <td class="text-right pb-3">
+                  <span class="text-sm lg:text-base font-bold text-darkColor">
                     50000
                   </span>
                 </td>
               </tr>
               <tr>
-                <td className="hidden pb-4 md:table-cell">
-                  <Link to="#">
+                <td class="hidden pb-4 md:table-cell">
+                  <a href="#">
                     <img
                       src="https://images.pexels.com/photos/102104/pexels-photo-102104.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-                      className="w-40 h-30 rounded"
+                      class="w-40 h-30 rounded"
                       alt="Thumbnail"
                     />
-                  </Link>
+                  </a>
                 </td>
                 <td>
-                  <Link to="#">
-                    <p className="mb-2 md:ml-4">Apel Fuji Organik</p>
-                  </Link>
+                  <a href="#">
+                    <p class="mb-2 md:ml-4">Apel Fuji Organik</p>
+                  </a>
                 </td>
-                <td className="hidden text-right md:table-cell pb-3">
-                  <span className="text-sm lg:text-base font-medium">2</span>
+                <td class="hidden text-right md:table-cell pb-3">
+                  <span class="text-sm lg:text-base font-medium">2</span>
                 </td>
-                <td className="hidden text-right md:table-cell pb-3">
-                  <span className="text-sm lg:text-base font-medium">10000</span>
+                <td class="hidden text-right md:table-cell pb-3">
+                  <span class="text-sm lg:text-base font-medium">10000</span>
                 </td>
-                <td className="text-right pb-3">
-                  <span className="text-sm lg:text-base font-bold text-darkColor">
+                <td class="text-right pb-3">
+                  <span class="text-sm lg:text-base font-bold text-darkColor">
                     20000
                   </span>
                 </td>
               </tr>
             </tbody>
           </table>
-          <hr className="pb-6 mt-6" />
-          <div className="flex px-2">
-            <div className="w-2/3">
-              <h1 className="font-semibold text-lg text-center">Buyer Details</h1>
-              <div className="py-5">
+          <hr class="pb-6 mt-6" />
+          <div class="flex px-2">
+            <div class="w-2/3">
+              <h1 class="font-semibold text-lg text-center">Buyer Details</h1>
+              <div class="py-5">
                 <table>
                   <tbody>
                     <tr>
-                      <td className="hidden pb-4 md:table-cell">
-                        <h1 className="font-semibold text-md">Name</h1>
+                      <td class="hidden pb-4 md:table-cell">
+                        <h1 class="font-semibold text-md">Name</h1>
                       </td>
                       <td>
-                        <Link to="#">
-                          <p className="mb-4 md:ml-4">Rakha Naufal Akbar Fikri</p>
-                        </Link>
+                        <a href="#">
+                          <p class="mb-4 md:ml-4">Rakha Naufal Akbar Fikri</p>
+                        </a>
                       </td>
                     </tr>
                     <tr>
-                      <td className="hidden pb-4 md:table-cell">
-                        <h1 className="font-semibold text-md">Email</h1>
+                      <td class="hidden pb-4 md:table-cell">
+                        <h1 class="font-semibold text-md">Email</h1>
                       </td>
                       <td>
-                        <Link to="#">
-                          <p className="mb-4 md:ml-4">rnaufalaf@gmail.com</p>
-                        </Link>
+                        <a href="#">
+                          <p class="mb-4 md:ml-4">rnaufalaf@gmail.com</p>
+                        </a>
                       </td>
                     </tr>
                     <tr>
-                      <td className="hidden pb-4 md:table-cell">
-                        <h1 className="font-semibold text-md">Address</h1>
+                      <td class="hidden pb-4 md:table-cell">
+                        <h1 class="font-semibold text-md">Address</h1>
                       </td>
                       <td>
-                        <Link to="#">
-                          <p className="mb-4 md:ml-4">
+                        <a href="#">
+                          <p class="mb-4 md:ml-4">
                             Hazlehurst Ln, Lightwood Ln, Sheffield S8 8BG,
                             United Kingdom
                           </p>
-                        </Link>
+                        </a>
                       </td>
                     </tr>
                     <tr>
-                      <td className="hidden pb-4 md:table-cell">
-                        <h1 className="font-semibold text-md">Phone</h1>
+                      <td class="hidden pb-4 md:table-cell">
+                        <h1 class="font-semibold text-md">Phone</h1>
                       </td>
                       <td>
-                        <Link to="#">
-                          <p className="mb-4 md:ml-4">+44 12345678</p>
-                        </Link>
+                        <a href="#">
+                          <p class="mb-4 md:ml-4">+44 12345678</p>
+                        </a>
                       </td>
                     </tr>
                   </tbody>
                 </table>
               </div>
             </div>
-            <div className="border border-1 mx-3" />
-            <div className="w-1/3">
-              <h1 className="font-semibold text-lg text-center">Subtotal</h1>
-              <div className="py-5">
+            <div class="border border-1 mx-3" />
+            <div class="w-1/3">
+              <h1 class="font-semibold text-lg text-center">Subtotal</h1>
+              <div class="py-5">
                 <table align="center">
                   <tbody>
                     <tr>
-                      <td className="hidden pb-4 md:table-cell">
-                        <h1 className="font-semibold text-md">Price</h1>
+                      <td class="hidden pb-4 md:table-cell">
+                        <h1 class="font-semibold text-md">Price</h1>
                       </td>
                       <td>
-                        <Link to="#">
-                          <p className="mb-4 md:ml-4 text-midColor font-semibold">
+                        <a href="#">
+                          <p class="mb-4 md:ml-4 text-midColor font-semibold">
                             100000
                           </p>
-                        </Link>
+                        </a>
                       </td>
                     </tr>
                     <tr>
-                      <td className="hidden pb-4 md:table-cell">
-                        <h1 className="font-semibold text-md">Discount</h1>
+                      <td class="hidden pb-4 md:table-cell">
+                        <h1 class="font-semibold text-md">Discount</h1>
                       </td>
                       <td>
-                        <Link to="#">
-                          <p className="mb-4 md:ml-4 font-semibold text-red-600">
+                        <a href="#">
+                          <p class="mb-4 md:ml-4 font-semibold text-red-600">
                             -4000
                           </p>
-                        </Link>
+                        </a>
                       </td>
                     </tr>
                     <tr>
-                      <td className="hidden pb-4 md:table-cell">
-                        <h1 className="font-semibold text-md">Tax</h1>
+                      <td class="hidden pb-4 md:table-cell">
+                        <h1 class="font-semibold text-md">Tax</h1>
                       </td>
                       <td>
-                        <Link to="#">
-                          <p className="mb-4 md:ml-4 font-semibold text-accentColor">
+                        <a href="#">
+                          <p class="mb-4 md:ml-4 font-semibold text-accentColor">
                             1000
                           </p>
-                        </Link>
+                        </a>
                       </td>
                     </tr>
                     <tr>
-                      <td className="hidden pb-4 md:table-cell">
-                        <h1 className="font-semibold text-md">Total</h1>
+                      <td class="hidden pb-4 md:table-cell">
+                        <h1 class="font-semibold text-md">Total</h1>
                       </td>
                       <td>
-                        <Link to="#">
-                          <p className="mb-4 md:ml-4 font-bold text-midColor">
+                        <a href="#">
+                          <p class="mb-4 md:ml-4 font-bold text-midColor">
                             7000
                           </p>
-                        </Link>
+                        </a>
                       </td>
                     </tr>
                   </tbody>
                 </table>
                 <div align="center">
                   <button
-                    className="text-lightColor bg-darkColor font-bold p-3 rounded mt-5"
-                    onClick={() => navigate("/user")}
+                    class="text-lightColor bg-darkColor font-bold p-3 rounded mt-5"
+                    onClick={() => setOpenModal(true)}
                   >
                     PAY NOW
                   </button>
@@ -206,7 +256,7 @@ const CheckoutPage = () => {
             </div>
           </div>
         </div>
-        <hr className="mx-5" />
+        <hr class="mx-5" />
       </div>
     </div>
   );
